2b51eaf49fbad89ca39503c2684838cc7b463b09
daver987 <daveyalexander@outlook.com>
2022-12-27 18:57:21 -0500

    change build preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
